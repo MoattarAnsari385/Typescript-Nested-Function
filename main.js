089bd9f7c8ef0b4bfef13dbcf805a14cf36f57ee
Moattar Ansari <moattaransari62@gmail.com>
2024-11-19 07:47:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+// Nested Function...
+// let newFunc = ()=>{
+// console.log("ArrowFunc")
+// }
+// let anOtherfunc = () => {
+//     newFunc()
+// }
+// anOtherfunc()
+let sum = () => {
+    return 7 + 7;
+};
+let total = sum();
+console.log(sum);
+// Nested function...
---
+++ main.ts
@@ -0,0 +1,25 @@
+// Nested Function...
+
+// let newFunc = ()=>{
+    // console.log("ArrowFunc")
+// }
+
+// let anOtherfunc = () => {
+//     newFunc()
+// }
+
+// anOtherfunc()
+
+
+
+let sum = ()=>{
+    return 7+7
+}
+
+let total = sum()
+
+console.log(sum);
+
+
+// Nested function...
+
